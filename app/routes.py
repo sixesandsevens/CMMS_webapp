6e553e18dc6a8732f267e65686c921491bdea5cc
sixesandsevens <darkseren@gmail.com>
2025-06-11 19:56:17 -0400

    idk

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from flask import render_template, flash, redirect, url_for, request, Blueprint
 from flask_login import current_user, login_user, logout_user, login_required
-from werkzeug.urls import url_parse
+from urllib.parse import urlparse
 from app import db
 from app.forms import LoginForm, RegistrationForm, AssetForm, WorkOrderForm
 from app.models import User, Asset, WorkOrder
@@ -27,7 +27,7 @@ def login():
             return redirect(url_for('main.login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
+        if not next_page or urlparse(next_page).netloc != '':
             next_page = url_for('main.index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
